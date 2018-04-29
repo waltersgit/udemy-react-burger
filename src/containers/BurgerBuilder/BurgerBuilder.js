6272c42b3dc998752752825a61b8830438e83930
peter <designs.peter@gmail.com>
2018-04-29 10:59:41 +0800

    lesson 348 測試reducer

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -19,7 +19,7 @@ const INGREDIENT_PRICES = {
     bacon: 0.7
 }
 
-class BurgerBuilder extends Component {
+export class BurgerBuilder extends Component {
     state = {
         purchasing: false
     }
